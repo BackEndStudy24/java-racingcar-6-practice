93b1a5cc5924313c077bb69215fb0429e6994012
Teddysir <sdzx0719@naver.com>
2024-12-11 13:53:19 +0900

    feat: 예외 처리 기능 적용

--- src/main/java/racingcar/controller/Controller.java
+++ src/main/java/racingcar/controller/Controller.java
@@ -1,18 +1,27 @@
 package racingcar.controller;
 
+import racingcar.utils.ServiceValidation;
 import racingcar.view.InputView;
 
 public class Controller {
 
     public void start() {
         clientInput_CarNames();
+        clientInput_MoveCount();
     }
 
     private void clientInput_CarNames() {
         String rawInput = InputView.getInput();
+        ServiceValidation.validateNull(rawInput);
     }
 
     private void clientInput_MoveCount() {
         String rawMoveCount = InputView.getMoveCount();
+        ServiceValidation.validateNull(rawMoveCount);
+        ServiceValidation.validateIsNumeric(rawMoveCount);
+        int moveCount = Integer.parseInt(rawMoveCount);
+        ServiceValidation.validateMoveCount(moveCount);
+
+
     }
 }
