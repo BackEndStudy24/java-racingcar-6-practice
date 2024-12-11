2f76a5c08bd9aed79105b5d9e72f453c0dbaeeff
Teddysir <sdzx0719@naver.com>
2024-12-11 13:21:24 +0900

    feat: 빈값의 자동차 이름일 경우 예외 처리 구현

--- src/main/java/racingcar/constants/ErrorMessageType.java
+++ src/main/java/racingcar/constants/ErrorMessageType.java
@@ -2,7 +2,8 @@ package racingcar.constants;
 
 public enum ErrorMessageType {
     INVALID_MOVE_COUNT("[ERROR] 올바르지 않은 전진 횟수입니다."),
-    INVALID_NULL_INPUT("[ERROR] 빈 값은 입력할 수 없습니다.");
+    INVALID_NULL_INPUT("[ERROR] 빈 값은 입력할 수 없습니다."),
+    INVALID_CAR_NAME("[ERROR] 자동차 이름은 공백이 될 수 없습니다.");
 
     private final String message;
 
--- src/main/java/racingcar/utils/ServiceValidation.java
+++ src/main/java/racingcar/utils/ServiceValidation.java
@@ -2,6 +2,7 @@ package racingcar.utils;
 
 import racingcar.constants.ErrorMessageType;
 
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class ServiceValidation {
@@ -17,6 +18,14 @@ public class ServiceValidation {
     public static void validateIsNumeric(String input) {
         if (!NUMBER_PATTERN.matcher(input).matches()) {
             throw new IllegalArgumentException(ErrorMessageType.INVALID_MOVE_COUNT.getMessage());
+        }
+    }
+
+    public static void validateCarNameNull(List<String> carNames) {
+        for(String car : carNames) {
+            if(car.isBlank()) {
+                throw new IllegalArgumentException(ErrorMessageType.INVALID_CAR_NAME.getMessage());
+            }
         }
     }
 
